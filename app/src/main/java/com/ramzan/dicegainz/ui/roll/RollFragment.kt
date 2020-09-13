811893942866d050e75d33c25f4d83fc8ed066fe
Ramzan <s.ramzan@protonmail.com>
2020-09-13 17:49:46 -0400

    Start roll ui

--- app/src/main/java/com/ramzan/dicegainz/ui/roll/RollFragment.kt
+++ app/src/main/java/com/ramzan/dicegainz/ui/roll/RollFragment.kt
@@ -4,30 +4,51 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.ramzan.dicegainz.R
+import com.ramzan.dicegainz.database.LiftDatabase
+import com.ramzan.dicegainz.databinding.RollFragmentBinding
 
 /**
  * A placeholder fragment containing a simple view.
  */
 class RollFragment : Fragment() {
 
+    private lateinit var binding: RollFragmentBinding
+
+    private lateinit var viewModel: RollViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.roll_fragment, container, false)
+
+        // Inflate view and get instance of binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.roll_fragment, container, false
+        )
+
+        // Get ViewModel Factory
+        val application = requireNotNull(this.activity).application
+        val dataSource = LiftDatabase.getInstance(application).liftDatabaseDao
+        val viewModelFactory = RollViewModelFactory(dataSource, application)
+
+        // Get ViewModel
+        viewModel = ViewModelProvider(this, viewModelFactory).get(RollViewModel::class.java)
+        binding.rollViewModel = viewModel
+
+        binding.lifecycleOwner = this
+
+
+        return binding.root
     }
 
     companion object {
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         @JvmStatic
-        fun newInstance(): RollFragment {
-            return RollFragment()
-        }
+        fun newInstance() =
+            RollFragment()
     }
 }
