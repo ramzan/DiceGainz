0242a7db780fdd8b8c77e8b69d784b76107b98d0
Ramzan <s.ramzan@protonmail.com>
2020-10-03 15:28:19 -0400

    Add rolling functionality and make roll fragment scrollable

--- app/src/main/java/com/ramzan/dicegainz/ui/main/MainViewModel.kt
+++ app/src/main/java/com/ramzan/dicegainz/ui/main/MainViewModel.kt
@@ -1,12 +1,11 @@
 package com.ramzan.dicegainz.ui.main
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.*
 import com.ramzan.dicegainz.database.Lift
 import com.ramzan.dicegainz.database.LiftDatabase
+import com.ramzan.dicegainz.database.T1
+import com.ramzan.dicegainz.database.T2
 import com.ramzan.dicegainz.repository.Repository
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
@@ -42,6 +41,35 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private var lifts2 = repo.getAllLifts()
     private var lifts3 = repo.getAllLifts()
 
+    private val combinedValues =
+        MediatorLiveData<Triple<List<Lift>?, List<Lift>?, List<Lift>?>>().apply {
+            addSource(lifts1) {
+                value = Triple(it, lifts2.value, lifts3.value)
+            }
+            addSource(lifts2) {
+                value = Triple(lifts1.value, it, lifts3.value)
+            }
+            addSource(lifts3) {
+                value = Triple(lifts1.value, lifts2.value, it)
+            }
+        }
+
+    val liftsLoaded = Transformations.map(combinedValues) { triple ->
+        triple.first != null && triple.second != null && triple.third != null
+    }
+
+    private var _lift1Text = MutableLiveData("")
+    private var _lift2Text = MutableLiveData("")
+    private var _lift3Text = MutableLiveData("")
+
+    val lift1Text: MutableLiveData<String>
+        get() = _lift1Text
+    val lift2Text: MutableLiveData<String>
+        get() = _lift2Text
+    val lift3Text: MutableLiveData<String>
+        get() = _lift3Text
+
+
     fun getAllLifts(liftNumber: Int) {
         when (liftNumber) {
             1 -> lifts1 = repo.getAllLifts()
@@ -55,6 +83,33 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
             1 -> lifts1 = repo.getLiftsForTag(tag)
             2 -> lifts2 = repo.getLiftsForTag(tag)
             3 -> lifts3 = repo.getLiftsForTag(tag)
+        }
+    }
+
+    fun roll(liftNumber: Int) {
+        when (liftNumber) {
+            1 -> updateLiftText(lifts1, _lift1Text)
+            2 -> updateLiftText(lifts2, _lift2Text)
+            3 -> updateLiftText(lifts3, _lift3Text)
+        }
+    }
+
+    fun rollAll() {
+        roll(1)
+        roll(2)
+        roll(3)
+    }
+
+    private fun updateLiftText(lifts: LiveData<List<Lift>>, liftText: MutableLiveData<String>) {
+        val lift = lifts.value!!.random()
+        liftText.value = "${lift.name} ${getRM(lift.tier)}RM"
+    }
+
+    private fun getRM(tier: Int): Int {
+        return when (tier) {
+            T1 -> (3..6).random()
+            T2 -> (6..10).random()
+            else -> (3..10).random()
         }
     }
 }
