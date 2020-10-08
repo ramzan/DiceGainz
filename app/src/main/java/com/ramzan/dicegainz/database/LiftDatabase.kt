dad8d4215a5abecff2ee36277566ed967e21ea22
Ramzan <s.ramzan@protonmail.com>
2020-10-08 14:05:33 -0400

    Prepopulate database

--- app/src/main/java/com/ramzan/dicegainz/database/LiftDatabase.kt
+++ app/src/main/java/com/ramzan/dicegainz/database/LiftDatabase.kt
@@ -4,6 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * A database that stores Lift information.
@@ -75,6 +80,21 @@ abstract class LiftDatabase : RoomDatabase() {
                         // migration with Room in this blog post:
                         // https://medium.com/androiddevelopers/understanding-migrations-with-room-f01e04b07929
                         .fallbackToDestructiveMigration()
+                        // prepopulate the database after onCreate was called
+                        .addCallback(object : Callback() {
+                            override fun onCreate(db: SupportSQLiteDatabase) {
+                                super.onCreate(db)
+                                // moving to a new thread
+                                GlobalScope.launch(Dispatchers.IO) {
+                                    withContext(Dispatchers.IO) {
+                                        getInstance(context).liftDatabaseDao.apply {
+                                            insertLifts(PREPOPULATE_LIFTS)
+                                            insertTags(PREPOPULATE_TAGS)
+                                        }
+                                    }
+                                }
+                            }
+                        })
                         .build()
                     // Assign INSTANCE to the newly created database.
                     INSTANCE = instance
@@ -84,5 +104,45 @@ abstract class LiftDatabase : RoomDatabase() {
                 return instance
             }
         }
+
+        val PREPOPULATE_LIFTS = listOf(
+            Lift(1, "Squat", BOTH),
+            Lift(2, "Bench Press", BOTH),
+            Lift(3, "Deadlift", BOTH),
+            Lift(4, "Overhead Press", BOTH),
+            Lift(5, "Front Squat", BOTH),
+            Lift(6, "Barbell Row", BOTH),
+            Lift(7, "Incline Press", BOTH),
+            Lift(8, "Pull Up", BOTH),
+            Lift(9, "Chin Up", BOTH),
+            Lift(10, "Dips", BOTH)
+        )
+        private const val PULL = "Pull"
+        private const val PUSH = "Push"
+        private const val LEGS = "Legs"
+        private const val UPPER = "Upper"
+        private const val LOWER = "Lower"
+        val PREPOPULATE_TAGS = listOf(
+            Tag(LEGS, 1),
+            Tag(LOWER, 1),
+            Tag(UPPER, 2),
+            Tag(PUSH, 2),
+            Tag(LEGS, 3),
+            Tag(LOWER, 3),
+            Tag(UPPER, 4),
+            Tag(PUSH, 4),
+            Tag(LEGS, 5),
+            Tag(LOWER, 5),
+            Tag(UPPER, 6),
+            Tag(PULL, 6),
+            Tag(UPPER, 7),
+            Tag(PUSH, 7),
+            Tag(UPPER, 8),
+            Tag(PULL, 8),
+            Tag(UPPER, 9),
+            Tag(PULL, 9),
+            Tag(UPPER, 10),
+            Tag(PUSH, 10)
+        )
     }
 }
