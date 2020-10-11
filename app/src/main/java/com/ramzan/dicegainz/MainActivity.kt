afd1f9600b20a2ed3e43d46ac9a2a741f8f13bf7
Ramzan <s.ramzan@protonmail.com>
2020-10-11 19:46:55 -0400

    Set app theme to follow system

--- app/src/main/java/com/ramzan/dicegainz/MainActivity.kt
+++ app/src/main/java/com/ramzan/dicegainz/MainActivity.kt
@@ -2,11 +2,13 @@ package com.ramzan.dicegainz
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         setContentView(R.layout.activity_main)
     }
 }
