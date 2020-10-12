7837c702b7f1a5dc8fc8150fcede32d849368b9d
Ramzan <s.ramzan@protonmail.com>
2020-10-12 18:46:48 -0400

    Add option to manually set theme

--- app/src/main/java/com/ramzan/dicegainz/MainActivity.kt
+++ app/src/main/java/com/ramzan/dicegainz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ramzan.dicegainz
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -8,7 +9,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        AppCompatDelegate.setDefaultNightMode(
+            sharedPref.getInt(getString(R.string.theme), AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        )
         setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/ramzan/dicegainz/ui/main/MainFragment.kt
+++ app/src/main/java/com/ramzan/dicegainz/ui/main/MainFragment.kt
@@ -1,11 +1,16 @@
 package com.ramzan.dicegainz.ui.main
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.ramzan.dicegainz.R
 import com.ramzan.dicegainz.databinding.MainFragmentBinding
 import com.ramzan.dicegainz.ui.SectionsPagerAdapter
@@ -30,6 +35,51 @@ class MainFragment : Fragment() {
             binding.tabs.setupWithViewPager(this)
         }
 
+        binding.mainToolbar.setOnMenuItemClickListener {
+            when (it) {
+                binding.mainToolbar.menu[0] -> showThemeDialog()
+                else -> false
+            }
+        }
+
         return binding.root
+    }
+
+    private val singleItems = arrayOf("Light", "Dark", "System")
+
+    private fun showThemeDialog(): Boolean {
+        val sharedPrefs = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        val themeString = getString(R.string.theme)
+        val oldTheme = sharedPrefs.getInt(themeString, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        val checkedItem = when (oldTheme) {
+            AppCompatDelegate.MODE_NIGHT_NO -> 0
+            AppCompatDelegate.MODE_NIGHT_YES -> 1
+            else -> 2
+        }
+        var newTheme = oldTheme
+
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(themeString)
+            .setPositiveButton(getString(R.string.ok)) { _, _ ->
+                // Respond to positive button press
+                if (newTheme != oldTheme) {
+                    with(sharedPrefs.edit()) {
+                        putInt(themeString, newTheme)
+                        apply()
+                    }
+                    startActivity(Intent.makeRestartActivityTask(activity?.intent?.component))
+                }
+            }
+            // Single-choice items (initialized with checked item)
+            .setSingleChoiceItems(singleItems, checkedItem) { _, which ->
+                // Respond to item chose
+                newTheme = when (which) {
+                    0 -> AppCompatDelegate.MODE_NIGHT_NO
+                    1 -> AppCompatDelegate.MODE_NIGHT_YES
+                    else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                }
+            }
+            .show()
+        return true
     }
 }
