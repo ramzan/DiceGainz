7183fb4b26894c4f8deae23834a057538bac003b
Ramzan Sheikh <s.ramzan@protonmail.com>
2021-06-15 12:40:46 -0400

    Remove confirmation button from theme dialog

--- app/src/main/java/com/nazmar/dicegainz/ui/dialogs/ThemeDialogFragment.kt
+++ app/src/main/java/com/nazmar/dicegainz/ui/dialogs/ThemeDialogFragment.kt
@@ -9,11 +9,7 @@ import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.nazmar.dicegainz.R
 
-const val THEME_PREF_KEY = "themeSelection"
-
 class ThemeDialogFragment : DialogFragment() {
-
-    private var newTheme = 0
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val themeStrings = resources.getStringArray(R.array.theme_string_array)
@@ -25,30 +21,21 @@ class ThemeDialogFragment : DialogFragment() {
             AppCompatDelegate.MODE_NIGHT_YES -> 1
             else -> 2
         }
-        newTheme = savedInstanceState?.getInt(THEME_PREF_KEY) ?: oldTheme
 
         return MaterialAlertDialogBuilder(requireContext())
             .setTitle(themeString)
-            .setPositiveButton(getString(R.string.ok)) { _, _ ->
-                if (newTheme != oldTheme) {
-                    sharedPrefs.edit {
-                        putInt(themeString, newTheme)
-                    }
-                    requireActivity().recreate()
-                }
-            }
             .setSingleChoiceItems(themeStrings, checkedItem) { _, which ->
-                newTheme = when (which) {
+                val newTheme = when (which) {
                     0 -> AppCompatDelegate.MODE_NIGHT_NO
                     1 -> AppCompatDelegate.MODE_NIGHT_YES
                     else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
                 }
+                if (newTheme != oldTheme) {
+                    sharedPrefs.edit { putInt(themeString, newTheme) }
+                    dismiss()
+                    requireActivity().recreate()
+                }
             }
             .show()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(THEME_PREF_KEY, newTheme)
     }
 }
